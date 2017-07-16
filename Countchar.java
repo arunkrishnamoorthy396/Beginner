5be0364855b5514a515d6be2044eeab200f5fed8
arunkrishnamoorthy396 <arunkrish4397@gmail.com>
2017-07-16 12:08:32 +0530

    Create Countchar.java

---
+++ Countchar.java
@@ -0,0 +1,12 @@
+package Beginner;
+
+import java.util.Scanner;
+
+public class Countchar {
+public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	String s1 = sc.next();
+	//char c1[] = s1.toCharArray();
+	System.out.println(s1.length());
+}
+}

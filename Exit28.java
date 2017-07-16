55834ec3dfdcc06c8f9f362b7df01e06461491d1
arunkrishnamoorthy396 <arunkrish4397@gmail.com>
2017-07-16 11:54:58 +0530

    Create Exit28.java

---
+++ Exit28.java
@@ -0,0 +1,13 @@
+package Beginner;
+
+import java.util.Scanner;
+
+public class Exit28 {
+public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	String c = sc.next();
+	if(c.equalsIgnoreCase("Q"))
+		System.out.println();System.exit(0);
+	
+}
+}
